039b04d312f25f44dc9174c2232a656158b42f60
liubing <liuskate@gmail.com>
2017-01-18 00:40:12 +0800

    Create shuffle-file.py
    shuffle file

---
+++ Python/FileTool/shuffle-file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import random, sys
+
+if len(sys.argv) < 3:
+  print 'Usage: python %s input output' % sys.argv[0]
+   sys.exit(-1)
+
+input, output = sys.argv[1], sys.argv[2]
+
+itemList = open(input).readlines()
+random.shuffle(itemList)
+fd = open(output, 'w+')
+for item in itemList:
+  fd.write('%s' % item)
+fd.close(
